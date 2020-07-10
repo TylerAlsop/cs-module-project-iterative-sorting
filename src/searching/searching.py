a4cb06314569e7ddf39222c219378ef24ef19ac0
Tyler Alsop <tylerjalsop@gmail.com>
2020-07-09 20:19:48 -0600

    linear_search complete, test passing. Might refactor.

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,5 +1,12 @@
 def linear_search(arr, target):
     # Your code here
+    index = 0
+
+    for i in arr:
+        if i == target:
+            return index
+        else:
+            index += 1
 
 
     return -1   # not found
